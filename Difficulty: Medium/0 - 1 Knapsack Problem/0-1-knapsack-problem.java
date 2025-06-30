9f9047365552a372d9d1c6fe6d34c6fd79197322
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-06-30 19:37:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -1,32 +1,36 @@
 class Solution {
-    public static int func(int idx,int W,int val[],int wt[],Integer dp[][])
-    {
-        if(idx == 0)
-        {
-            if(wt[idx] <= W)
-            {
-                return val[idx];
-            }else
-            {
-                return 0;
-            }
-        }
-        if(dp[idx][W] != null)
-        {
-            return dp[idx][W];
-        }
-        int notpick=0+func(idx-1,W,val,wt,dp);
-        int pick=Integer.MIN_VALUE;
-        if(W >= wt[idx])
-        {
-            pick=val[idx]+func(idx-1,W-wt[idx],val,wt,dp);
-        }
-        return dp[idx][W]=Math.max(pick,notpick);
-    }
+   
     static int knapsack(int W, int val[], int wt[]) {
         // code here
         int n=val.length;
         Integer dp[][]=new Integer[val.length][W+1];
-        return func(n-1,W,val,wt,dp);
+        for(int i=0;i<=W;i++)
+        {
+            if(i>= wt[0])
+            {
+                dp[0][i]=val[0];
+            }else
+            {
+                dp[0][i]=0;
+            }
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<=W;j++)
+            {
+                int notpick=0+dp[i-1][j];
+                int pick=0;
+                if(j >= wt[i])
+                {
+                    pick=val[i]+dp[i-1][j-wt[i]];
+                }
+                dp[i][j]=Math.max(pick,notpick);
+            }
+        }
+        // for(int i=0;i<n;i++)
+        // {
+        // System.out.println(Arrays.toString(dp[i]));
+        // }
+        return dp[n-1][W];
     }
 }
