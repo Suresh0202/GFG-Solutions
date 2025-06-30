797801fc26be30ca863a99316ecc3b72e58a36cb
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-06-30 19:49:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -1,12 +1,11 @@
 class Solution {
-   
     static int knapsack(int W, int val[], int wt[]) {
         // code here
-        int n=val.length;
         Integer dp[][]=new Integer[val.length][W+1];
+        int n=val.length;
         for(int i=0;i<=W;i++)
-        {
-            if(i>= wt[0])
+        {   
+            if(i >= wt[0])
             {
                 dp[0][i]=val[0];
             }else
@@ -18,19 +17,15 @@ class Solution {
         {
             for(int j=0;j<=W;j++)
             {
-                int notpick=0+dp[i-1][j];
+                int npick=dp[i-1][j];
                 int pick=0;
                 if(j >= wt[i])
                 {
                     pick=val[i]+dp[i-1][j-wt[i]];
                 }
-                dp[i][j]=Math.max(pick,notpick);
+                dp[i][j]=Math.max(pick,npick);
             }
         }
-        // for(int i=0;i<n;i++)
-        // {
-        // System.out.println(Arrays.toString(dp[i]));
-        // }
         return dp[n-1][W];
     }
 }
