93f12b9f95402e59df1076bfb207eb6da028d380
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-08-06 19:10:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Smallest Positive Missing/smallest-positive-missing.java
+++ Difficulty: Medium/Smallest Positive Missing/smallest-positive-missing.java
@@ -2,24 +2,23 @@ class Solution {
     public int missingNumber(int[] arr) {
         // code here
         HashSet<Integer>st=new HashSet<>();
+        int max=0;
         for(int i:arr)
         {
             if(i>0)
             {
                 st.add(i);
             }
-            
+            max=Math.max(max,i);
         }
-        int c=0;
         for(int i=1;i<=arr.length;i++)
         {
             if(!st.contains(i))
             {
                 return i;
             }
-            c++;
         }
-        // if(c )
-        return arr.length+1;
+        return max+1;
+        
     }
 }
