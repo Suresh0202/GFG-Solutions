11ecc7c00a2543a571db5f914d1e7a1b01c393ac
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-08-30 19:52:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge K sorted linked lists/merge-k-sorted-linked-lists.java
@@ -0,0 +1,18 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    Node mergeKLists(Node[] arr) {
+       PriorityQueue<
+        
+    }
+}
