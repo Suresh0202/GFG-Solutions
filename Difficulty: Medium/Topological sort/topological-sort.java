9dc74cf8b6cd54740704c48d1b9bd5562294be80
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-09-10 17:23:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.java
+++ Difficulty: Medium/Topological sort/topological-sort.java
@@ -1,98 +1,42 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-
-        while (t-- > 0) {
-            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-            int vertices = Integer.parseInt(read.readLine());
-            int edges = Integer.parseInt(read.readLine());
-
-            for (int i = 0; i < vertices; i++) adj.add(i, new ArrayList<Integer>());
-
-            int p = 0;
-            for (int i = 1; i <= edges; i++) {
-                String s[] = read.readLine().trim().split("\\s+");
-                int u = Integer.parseInt(s[0]);
-                int v = Integer.parseInt(s[1]);
-                adj.get(u).add(v);
-            }
-
-            ArrayList<Integer> res = new Solution().topologicalSort(adj);
-
-            if (check(adj, vertices, res) == true)
-                System.out.println("1");
-            else
-                System.out.println("0");
-            System.out.println("~");
-        }
-    }
-
-    static boolean check(ArrayList<ArrayList<Integer>> adj, int V,
-                         ArrayList<Integer> res) {
-
-        if (V != res.size()) return false;
-
-        int[] map = new int[V];
-        for (int i = 0; i < V; i++) {
-            map[res.get(i)] = i;
-        }
-        for (int i = 0; i < V; i++) {
-            for (int v : adj.get(i)) {
-                if (map[i] > map[v]) return false;
-            }
-        }
-        return true;
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
-    // Function to return list containing vertices in Topological order.
-    static ArrayList<Integer> topologicalSort(ArrayList<ArrayList<Integer>> adj) {
-        // Your code here
-        // step 1:find indergree of graph
-        int indeg[]=new int[adj.size()];
-        for(int i=0;i<adj.size();i++)
+    public static void dfs(int node,ArrayList<ArrayList<Integer>>adj,ArrayList<Integer>ans,int vis[]){
+        vis[node]=1;
+        for(int it:adj.get(node))
         {
-            for(int it:adj.get(i))
+            if(vis[it] == -1)
             {
-                indeg[it]++;
+                vis[it]=1;
+                dfs(it,adj,ans,vis);
             }
-            
         }
-        Queue<Integer>qu=new LinkedList<>();
+        ans.add(0,node);
+    }
+    public static ArrayList<Integer> topoSort(int V, int[][] edges) {
+        // code here
+        ArrayList<ArrayList<Integer>>adj=new ArrayList<>();
+        for(int i=0;i<V;i++)
+        {
+            adj.add(new ArrayList<>());
+        }
+       for(int e[]:edges)
+       {
+           int u=e[0];
+           int v=e[1];
+           adj.get(u).add(v);
+           
+       }
+       ArrayList<Integer>ans=new ArrayList<>();
+        int vis[]=new int[V];
+        Arrays.fill(vis,-1);
+        for(int i=0;i<V;i++)
+        {
+            if(vis[i] == -1)
+            {
+                dfs(i,adj,ans,vis);
+                vis[i]=1;
+            }
+        }
+        return ans;
         
-        // step 2:add all nodes in qu with indeg 0:
-        for(int i=0;i<indeg.length;i++){
-            if(indeg[i]==0)
-            {
-                qu.add(i);
-            }
-        }
-        // step 3:apply bfs and find the topo order 
-        ArrayList<Integer>topo=new ArrayList<>();
-        while(!qu.isEmpty())
-        {
-            int node=qu.peek();
-            qu.remove();
-            topo.add(node);
-            for(int i:adj.get(node))
-            {
-                indeg[i]--;
-                if(indeg[i]==0)
-                {
-                    qu.add(i);
-                }
-            }
-        }
-        return topo;
     }
 }
