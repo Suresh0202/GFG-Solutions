0304b0d273b3c99a9ebba339898c85ff27cc9889
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-09-10 17:08:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find the number of islands/find-the-number-of-islands.java
+++ Difficulty: Medium/Find the number of islands/find-the-number-of-islands.java
@@ -1,76 +1,43 @@
-//{ Driver Code Starts
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int tc = scanner.nextInt();
-        while (tc-- > 0) {
-            int n = scanner.nextInt();
-            int m = scanner.nextInt();
-            char[][] grid = new char[n][m];
-
-            // Read the grid input
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    grid[i][j] = scanner.next().charAt(0);
-                }
-            }
-            Solution obj = new Solution();
-            int ans = obj.countIslands(grid);
-            System.out.println(ans);
-            System.out.println("~");
-        }
-        scanner.close();
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
-    public void dfs(int sr,int sc,char grid[][],int vis[][])
+    public void dfs(int sr,int sc,int n,int m,int vis[][],char grid[][])
     {
-        if(sr<0 || sc<0 || sr>=grid.length || sc>=grid[0].length || vis[sr][sc] != -1 )
+        if(sr<0 || sc<0 || sr>=n || sc>=m || vis[sr][sc] != -1 || grid[sr][sc] !='L')
         {
             return;
         }
-        if(vis[sr][sc]==-1 && grid[sr][sc]=='L')
-        {
-            vis[sr][sc]=1;
-            dfs(sr-1,sc,grid,vis);
-            dfs(sr+1,sc,grid,vis);
-            dfs(sr,sc-1,grid,vis);
-            dfs(sr,sc+1,grid,vis);
-            dfs(sr-1,sc-1,grid,vis);
-            dfs(sr+1,sc+1,grid,vis);
-            dfs(sr-1,sc+1,grid,vis);
-            dfs(sr+1,sc-1,grid,vis);
-        }
+        vis[sr][sc]=1;
+        dfs(sr+1,sc+1,n,m,vis,grid);
+        dfs(sr-1,sc-1,n,m,vis,grid);
+        dfs(sr+1,sc-1,n,m,vis,grid);
+        dfs(sr-1,sc+1,n,m,vis,grid);
+        dfs(sr+1,sc,n,m,vis,grid);
+        dfs(sr,sc+1,n,m,vis,grid);
+        dfs(sr,sc-1,n,m,vis,grid);
+        dfs(sr-1,sc,n,m,vis,grid);
+        
     }
     public int countIslands(char[][] grid) {
         // Code here
         int n=grid.length;
         int m=grid[0].length;
-        int ans=0;
         int vis[][]=new int[n][m];
         for(int i=0;i<n;i++)
         {
             Arrays.fill(vis[i],-1);
         }
+        int cnt=0;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(grid[i][j] == 'L' && vis[i][j]==-1)
+                if(vis[i][j] == -1 && grid[i][j] == 'L')
                 {
-                    ans++;
-                    dfs(i,j,grid,vis);
+                    dfs(i,j,n,m,vis,grid);
+                    cnt++;
                 }
             }
         }
-       
-        return ans;
         
+        return cnt;
     }
 }
