60d88ffa0d8bfcc10a48a31b272ac9c367d51cb1
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-10-18 12:44:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/k-th Smallest in BST/kth-smallest-in-bst.java
+++ Difficulty: Medium/k-th Smallest in BST/kth-smallest-in-bst.java
@@ -1,96 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-/*package whatever //do not write package name here */
-
-import java.io.*;
-import java.math.*;
-import java.util.*;
-
-class Node {
-    int data;
-    Node left, right;
-
-    public Node(int d) {
-        data = d;
-        left = right = null;
-    }
-}
-
-class GFG {
-    static Node buildTree(String str) {
-        // Corner Case
-        if (str.length() == 0 || str.equals('N')) return null;
-        String[] s = str.split(" ");
-
-        Node root = new Node(Integer.parseInt(s[0]));
-        Queue<Node> q = new LinkedList<Node>();
-        q.add(root);
-
-        // Starting from the second element
-        int i = 1;
-        while (!q.isEmpty() && i < s.length) {
-            // Get and remove the front of the queue
-            Node currrNode = q.remove();
-
-            // Get the currrent node's value from the string
-            String currrVal = s[i];
-
-            // If the left child is not null
-            if (!currrVal.equals("N")) {
-
-                // Create the left child for the currrent node
-                currrNode.left = new Node(Integer.parseInt(currrVal));
-
-                // Push it to the queue
-                q.add(currrNode.left);
-            }
-
-            // For the right child
-            i++;
-            if (i >= s.length) break;
-            currrVal = s[i];
-
-            // If the right child is not null
-            if (!currrVal.equals("N")) {
-
-                // Create the right child for the currrent node
-                currrNode.right = new Node(Integer.parseInt(currrVal));
-
-                // Push it to the queue
-                q.add(currrNode.right);
-            }
-
-            i++;
-        }
-
-        return root;
-    }
-
-    public static void main(String args[]) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim());
-        while (t > 0) {
-            String s = br.readLine();
-            Node root = buildTree(s);
-
-            int k = Integer.parseInt(br.readLine().trim());
-
-            Solution T = new Solution();
-            System.out.println(T.kthSmallest(root, k));
-            t--;
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 // class Node
 // {
 //     int data;
@@ -104,22 +11,33 @@ class GFG {
 // }
 
 class Solution {
-     int ans,cnt;
-     public void inorder(Node root,int k){
-        if(root==null) return;
-        inorder(root.left,k);
+    int res=-1;
+    int cnt=0;
+    public void func(Node root,int k)
+    {
+        if(root == null)
+        {
+            return ;
+        }
+        func(root.left,k);
         cnt++;
-        if(k==cnt){
-            ans=root.data;
+        if(cnt == k)
+        {
+            res=root.data;
             return;
         }
-        inorder(root.right,k);
+        // arr.add(root.data);
+        func(root.right,k);
     }
     // Return the Kth smallest element in the given BST
     public int kthSmallest(Node root, int k) {
         // Write your code here
-          ans=-1; cnt=0;
-        inorder(root,k);
-        return ans;
+        if(root == null)
+        {
+            return -1;
+        }
+        // List<Integer>arr=new ArrayList<>();
+        func(root,k);
+        return res;
     }
 }
