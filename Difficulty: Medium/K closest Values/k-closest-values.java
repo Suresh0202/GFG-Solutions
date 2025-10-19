11a73375c6b43deb5d89ed476a79e7ed98a2e0f9
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-10-19 13:43:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K closest Values/k-closest-values.java
@@ -0,0 +1,57 @@
+/*
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+
+class Solution {
+    public void func(Node root,TreeMap<Integer,List<Integer>>mp,int tar)
+    {
+        if(root == null)
+        {
+            return;
+        }
+        func(root.left,mp,tar);
+        int diff=Math.abs(tar-root.data);
+        if(!mp.containsKey(diff))
+        {
+            mp.put(diff,new ArrayList<>());
+        }
+        mp.get(diff).add(root.data);
+        func(root.right,mp,tar);
+    }
+    public ArrayList<Integer> getKClosest(Node root, int target, int k) {
+        // code here
+        TreeMap<Integer,List<Integer>>mp=new TreeMap<>();
+        func(root,mp,target);
+        // System.out.println(mp);
+        ArrayList<Integer>ans=new ArrayList<>();
+        for(int val:mp.keySet())
+        {
+            List<Integer>ar=mp.get(val);
+            // System.out.println(ar);
+            for(int i=0;i<ar.size();i++)
+            {
+                if(ans.size()  < k)
+                {
+                ans.add(ar.get(i));
+                    // break;
+                }
+            }
+            if(ans.size() > k)
+            {
+                break;
+            }
+        }
+        return ans;
+        
+    }
+}
